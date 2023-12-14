76c3388b118b440e164e9dc93d6189cac07207fa
kotori <admin@0x.mk>
2023-12-14 13:11:04 -0500

    fix noreturn warning

--- CXXHook.cpp
+++ CXXHook.cpp
@@ -71,18 +71,19 @@ namespace {
 }
 
 extern "C" {
-inline void cxa_throw(void *ex, std::type_info *info, void (*dest)(void *)) {
+[[noreturn]] inline void cxa_throw(void *ex, std::type_info *info, void (*dest)(void *)) {
     std::string exception_name = demangle(reinterpret_cast<const std::type_info*>(info)->name());
     CXXException::StackTraceSaver::instance()->insert(ex, exception_name);
 
     static auto rethrow = reinterpret_cast<void (*)(void*,std::type_info *,void(*)(void*))>(dlsym(RTLD_NEXT, "__cxa_throw"));
     rethrow(ex,info,dest);
+    abort();  // make compiler happy
 }
 
 #if !__clang__
 void __cxa_throw(void *ex, void* info, void (*dest)(void*)) { cxa_throw(ex, reinterpret_cast<std::type_info *>(info), dest); }
 #else
-void __cxa_throw(void *ex, std::type_info* info, void (*dest)(void*)) { cxa_throw(ex, info, dest) }
+void __cxa_throw(void *ex, std::type_info* info, void (*dest)(void*)) { cxa_throw(ex, info, dest); }
 #endif
 
 }
